a007b7b93b10064b307561a41532cb1c501d4f82
jtaibo <jtaibo@95513402-c981-41be-8593-58c72ecd7a05>
2010-08-19 10:35:12 +0000

    Redefinition of directory structure. Adding Linux support.

--- src/maya2osg.cpp
+++ src/maya2osg.cpp
@@ -17,6 +17,7 @@
     You should have received a copy of the GNU General Public License
     along with Maya2OSG.  If not, see <http://www.gnu.org/licenses/>.
 */
+#include <maya/MArrayDataBuilder.h>
 #include <maya/MSimple.h>
 #include <maya/MGlobal.h>
 #include <maya/MSelectionList.h>
