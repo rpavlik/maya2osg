3520c11096d3cc66344e27b202eb5ecc7157d5f9
jtaibo <jtaibo@95513402-c981-41be-8593-58c72ecd7a05>
2010-09-08 17:33:07 +0000

    Automated installation for current user after compilation in Visual Studio. Installation batch file for Windows (INSTALL.bat)

---
+++ maya2009/INSTALL.bat
@@ -0,0 +1,15 @@
+set MAYA_VERSION=2009
+
+REM Copy the plug-in binary (maya2osg.mll)
+copy bin\maya2osg.mll "%PROGRAMFILES%\Autodesk\Maya%MAYA_VERSION%\bin\plug-ins\"
+
+REM Copy plug-in associated files (MEL scripts, shelf buttons, ...)
+if exist "%USERPROFILE%\Documents" GOTO ELSE
+	set DOCFILES="%USERPROFILE%\My Documents"
+:ELSE
+	set DOCFILES="%USERPROFILE%\Documents"
+:ENDIF
+
+copy scripts\*.mel %DOCFILES%\maya\%MAYA_VERSION%\scripts
+copy prefs\icons\*.bmp %DOCFILES%\maya\%MAYA_VERSION%\prefs\icons
+copy prefs\shelves\*.mel %DOCFILES%\maya\%MAYA_VERSION%\prefs\shelves
